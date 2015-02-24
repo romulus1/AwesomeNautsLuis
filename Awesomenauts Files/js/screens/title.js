552641afda4bb6aa5cf28e00b6cd9a2302d1a134
Luis Cedano <king.romanus1@gmail.com>
2015-02-23 20:39:36 -0800

    Video 27

--- Awesomenauts Files/js/screens/title.js
+++ Awesomenauts Files/js/screens/title.js
@@ -5,6 +5,25 @@ game.TitleScreen = me.ScreenObject.extend({
 	onResetEvent: function() {	
 		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
 		
+                me.input.bindKey(me.input.KEY.ENTER, "start");
+                
+                me.game.world.addChild(new (me.Renderable.extend({
+                    init: function(){
+                        this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+                        this.font = new me.Font("Arial", 46, "white");
+                    },
+                    
+                    draw: function(renderer){
+                        this.font.draw(renderer.getContext(), "Awesomenauts!", 350, 130);
+                        this.font.draw(renderer.getContext(), "Press ENTER to play!", 290, 400);
+                    }
+                })));
+                
+                this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){//event handler listens for when someone presses the enter button
+                    if(action === "start"){
+                        me.state.change(me.state.PLAY);
+                    }
+                }); 
 
 	},
 	
@@ -13,6 +32,7 @@ game.TitleScreen = me.ScreenObject.extend({
 	 *  action to perform when leaving this screen (state change)
 	 */
 	onDestroyEvent: function() {
-		; // TODO
+            me.input.unbindKey(me.input.KEY.ENTER);
+            me.event.unsubscribe(this.handler);
 	}
 });
