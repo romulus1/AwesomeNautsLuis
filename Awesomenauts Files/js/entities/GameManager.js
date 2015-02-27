8d479ef6d85e184d3fb40996679da6f7e23771c5
Luis Cedano <king.romanus1@gmail.com>
2015-02-26 19:33:15 -0800

    Video 29

---
+++ Awesomenauts Files/js/entities/GameManager.js
@@ -0,0 +1,37 @@
+game.GameManager = Object.extend({
+    init: function(x, y, settings) {
+        this.now = new Date().getTime();
+        this.paused = false;
+        this.lastCreep = new Date().getTime();
+
+        this.alwaysUpdate = true;
+    },
+    update: function() {
+        this.now = new Date().getTime();
+
+        if (game.data.player.dead) {
+            me.game.world.removeChild(game.data.player);
+            me.state.current().resetPlayer(10, 0);
+        }
+
+
+        if (Math.round(this.now / 1000) % 20 === 0 && (this.now - this.lastCreep >= 1000)) {
+            // this.lastCreep = this.now;
+            // var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
+            // me.game.world.addChild(creepe, 5);
+            // var creepe2 = me.pool.pull("MyCreep", 100, 0, {});
+            // me.game.world.addChild(creepe2, 5);
+            game.data.gold += 1;
+            console.log("Current gold count:" + game.data.gold);
+        }
+
+        if (Math.round(this.now / 1000) % 10 === 0 && (this.now - this.lastCreep >= 1000)) {
+            this.lastCreep = this.now;
+            var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
+            me.game.world.addChild(creepe, 5);
+            var creepe2 = me.pool.pull("MyCreep", 100, 0, {});
+            me.game.world.addChild(creepe2, 5);
+        }
+        return true;
+    }
+});
