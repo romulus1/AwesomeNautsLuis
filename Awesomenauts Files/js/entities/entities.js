5ee26068c6025c5a1e2957116a2e66b656304e2d
Luis Cedano <king.romanus1@gmail.com>
2015-02-27 11:28:54 -0800

    Video 29 completed

--- Awesomenauts Files/js/entities/entities.js
+++ Awesomenauts Files/js/entities/entities.js
@@ -1,7 +1,21 @@
 game.PlayerEntity = me.Entity.extend({
     init: function(x, y, settings) {
+        this.setSuper();
+        this.setPlayerTimers();
+        this.setAttributes();  
+        this.type = "PlayerEntity";
+        this.setFlags();
+
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+
+        this.addAnimations();
+        
+        this.renderable.setCurrentAnimation("idle");
+    },
+    
+    setSuper: function(){
         this._super(me.Entity, 'init', [x, y, {
-                //all dis makes sure that the sprite works
+                //all this makes sure that the sprite works
                 image: "player",
                 width: 64,
                 height: 64,
@@ -11,26 +25,35 @@ game.PlayerEntity = me.Entity.extend({
                     return(new me.Rect(0, 0, 64, 64)).toPolygon();//zeros are top corners and the two sixty fours are the width and the height
                 }
             }]);
-        this.type = "PlayerEntity";
-        this.health = game.data.playerHealth;
-        this.body.setVelocity(game.data.playerMoveSpeed, 20);//sets the velocity for the key binded
-        //keeps track of what direction your character is going
-        this.facing = "right";
+    },
+    
+    setPlayerTimers: function(){
         this.now = new Date().getTime();
         this.lastHit = this.now;
-        this.dead = false;
+        this.lastAttack = new Date().getTime();
+    },
+    
+    setAttributes: function(){
+        this.health = game.data.playerHealth;
+        this.body.setVelocity(game.data.playerMoveSpeed, 20);//sets the velocity for the key binded
         this.attack = game.data.playerAttack;
-        this.lastAttack = new Date().getTime(); //Havent used this
-        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
-
+    },
+    
+    setFlags: function(){      
+        //keeps track of what direction your character is going
+        this.facing = "right";
+        this.dead = false;
+    },
+    
+    addAnimations: function(){             
         this.renderable.addAnimation("idle", [78]);
         this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 123, 124, 125], 80);
         this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
-
-        this.renderable.setCurrentAnimation("idle");
     },
+    
     update: function(delta) {
         this.now = new Date().getTime();
+        
         if (this.health <= 0) {
             this.dead = true;
             // this.pos.x = 10;
